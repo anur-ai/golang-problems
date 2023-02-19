c96f8423505e34db61de16fdd15568d702800e99
Amir <amirabdunabiev2007@gmail.com>
2023-02-20 00:09:18 +0300

    10.go

--- begin/10.go
+++ begin/10.go
@@ -8,7 +8,7 @@ import (
 func main(){
 	var a, b float64
 	fmt.Scan(&a, &b)
-	if a > 0 && b > 0{
+	if a != 0 && b != 0{
 		fmt.Println(math.Pow(a, 2) - math.Pow(b, 2), math.Pow(a, 2) + math.Pow(b, 2), math.Pow(a, 2) * math.Pow(b, 2), math.Pow(a, 2) / math.Pow(b, 2))
 	}
 } 
