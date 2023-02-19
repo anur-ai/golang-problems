0a390d95ae698abc2f18690015ba4da02618cc94
Amir <amirabdunabiev2007@gmail.com>
2023-02-19 22:26:05 +0300

    8.go

--- begin/8.go
+++ begin/8.go
@@ -2,13 +2,8 @@ package main
 
 import "fmt"
 
-func main() {
-	var a, b float64
-
-	fmt.Println("Enter a, b")
-	fmt.Scanln(&a, &b)
-
-	s := (a + b) / 2
-
-	fmt.Println(s)
+func main(){
+	var a, b int
+	fmt.Scan(&a, &b)
+	fmt.Println((a + b) / 2) 
 }
