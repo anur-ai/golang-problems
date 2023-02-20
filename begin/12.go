6260a014b88ca34c1763d859a980125c2082c987
Amir <amirabdunabiev2007@gmail.com>
2023-02-20 18:45:25 +0300

    12.go

--- begin/12.go
+++ begin/12.go
@@ -5,22 +5,11 @@ import (
 	"math"
 )
 
-func hype(num1 float64, num2 float64) (float64) {
-	var result float64
-
-	result = math.Sqrt(math.Pow(num1, 2) + math.Pow(num2, 2))
-
-	return result
-}
-
-func main() {
-	var a, b, c float64
-
-	fmt.Println("Enter a, b")
-	fmt.Scanln(&a, &b)
-
-	c = hype(a, b)
-	p := a + b + c
-
-	fmt.Printf("%g, %g", c, p)
+func main(){
+	var a, b float64
+	fmt.Scan(&a, &b)
+	c := math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+	P := a + b + c
+	fmt.Println("c = ", c)	 
+	fmt.Println("P = ", P)	 
 }
