dd500712ba21dbf2870af44338f68ff156cc8a54
Amir <amirabdunabiev2007@gmail.com>
2023-02-20 20:53:42 +0300

    14.go

--- begin/14.go
+++ begin/14.go
@@ -5,16 +5,12 @@ import (
 	"math"
 )
 
-func main() {
-	var l, r float64
-	p := 3.14
-
-	fmt.Print("Enter L: ")
-	fmt.Scanln(&l)
-
-	r = l / 2 / p
-
-	s := p * math.Pow(r, 2)
-
-	fmt.Print("S=", float32(s), " R=", float32(r))
+func main(){
+	var L float64
+	fmt.Scan(&L)
+	P := 3.14
+	R := L / (2 * P) 
+	S := P * math.Pow(R, 2)
+	fmt.Println("R = ", R)
+	fmt.Println("S = ", S)
 }
