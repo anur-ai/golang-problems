8861a00a333a0651c2e10070d4468b5540048aa9
Amir <amirabdunabiev2007@gmail.com>
2023-02-20 21:08:40 +0300

    16.go

--- begin/16.go
+++ begin/16.go
@@ -5,10 +5,8 @@ import (
 	"math"
 )
 
-func main() {
+func main(){
 	var x1, x2 float64
-
-	fmt.Print("Enter x1 and x2 ")
-	fmt.Scanln(&x1, &x2)
-	fmt.Println(math.Abs(x1 - x2))
+	fmt.Scan(&x1, &x2)
+	fmt.Println(math.Abs(x2 - x1))
 }
