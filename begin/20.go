6572c740903b488cd884d73277b6ec14bc66a359
Amir <amirabdunabiev2007@gmail.com>
2023-02-21 17:57:13 +0300

    20.go

--- begin/20.go
+++ begin/20.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main(){
 	var x1, y1, x2, y2 float64
 	fmt.Scan(&x1, &y1)
 	fmt.Scan(&x2, &y2)
+	fmt.Println(math.Sqrt(math.Pow((x2 - x1), 2) - math.Pow((y2 - y1), 2)))
 }
