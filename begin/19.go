85666de3e2789c3a581a89136ec5c682c80570d5
Amir <amirabdunabiev2007@gmail.com>
2023-02-21 17:42:06 +0300

    19.go

--- begin/19.go
+++ begin/19.go
@@ -5,9 +5,10 @@ import (
 	"math"
 )
 
-func main(){
+func main() {
 	var x1, y1, x2, y2 float64
-	fmt.Scanln(&x1, &y1, &x2, y2)	
+	fmt.Scan(&x1, &y1)
+	fmt.Scan(&x2, &y2)
 	height := math.Abs(y2 - y1)
 	width := math.Abs(x2 - x1)
 	area := height * width
--- begin/20.go
+++ begin/20.go
@@ -1,18 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func main() {
+func main(){
 	var x1, y1, x2, y2 float64
-
-	fmt.Print("Enter x1 and y1 ")
-	fmt.Scanln(&x1, &y1)
-	fmt.Print("Enter x2 and y2 ")
-	fmt.Scanln(&x2, &y2)
-
-	fmt.Println(float32(math.Sqrt(math.Pow(x2 - x1, 2) + math.Pow(y2 - y1, 2))))
-
+	fmt.Scan(&x1, &y1)
+	fmt.Scan(&x2, &y2)
 }
